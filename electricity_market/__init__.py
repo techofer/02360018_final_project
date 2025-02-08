cc13d384476c4abb84c4be326c839c6a803987ca
Ofer Katz <ofer.katz@campus.technion.ac.il>
2025-02-09 00:48:25 +0200

    wip

--- electricity_market/__init__.py
+++ electricity_market/__init__.py
@@ -1,8 +1,1 @@
 __version__ = "0.0.1"
-
-from gymnasium.envs.registration import register
-
-register(
-    id='ElectricityMarketEnv-v0',
-    entry_point='electricity_market.electricity_market_env:ElectricityMarketEnv',
-)
